821b7ac208a6d4f61a2c77c7b377a49f16d3b3cd
Prathamesh Shiralkar <47828215+pnshiralkar@users.noreply.github.com>
2019-05-04 14:35:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import subprocess
 import os
 
-subprocess.call("npm install express\nnpm install python-shell\nnpm install ejs\nnode cppide.js", shell=True)
+subprocess.call("npm install express\nnpm install python-shell\nnpm install ejs\nnpm install uniqid\nnpm install rimraf\nnode cppide.js", shell=True)
